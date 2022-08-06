2917dbfd96534841fa80a6e1bafd8aa7fa928ecd
Yavuz Çakmak <yavuz.cakmak@affintybox.com>
2022-08-06 12:37:30 +0300

    ServiceImplementations

---
+++ NLayer.Service/Services/Service.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Services
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        private readonly IGenericRepository<T> _repoistory;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public Service(IGenericRepository<T> repoistory, IUnitOfWork unitOfWork)
+        {
+            _repoistory = repoistory;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _repoistory.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _repoistory.AddRangeAsync(entities);
+            await _unitOfWork.CommitAsync();
+            return entities;
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _repoistory.AnyAsync(expression);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _repoistory.GetAll().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int Id)
+        {
+            return await _repoistory.GetByIdAsync(Id); 
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            _repoistory.Remove(entity);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task RemoveRangeAsync(IEnumerable<T> entities)
+        {
+            _repoistory.RemoveRange(entities);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _repoistory.Update(entity);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return _repoistory.Where(expression);
+        }
+    }
+}
